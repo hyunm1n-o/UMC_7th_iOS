ba2627415052549b94cb71657571de4d0d5c6c78
Hamm1n <162417990+hyunm1n-o@users.noreply.github.com>
2024-10-07 20:16:12 +0900

    1주차

---
+++ CountUpDown/CountUpDown/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CountUpDown
+//
+//  Created by 오현민 on 9/23/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
